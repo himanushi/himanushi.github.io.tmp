e29c944c98bd43fa8459aaf9c61c83e16f613b8e
himanushi <himanushi@users.noreply.github.com>
2024-01-03 18:59:39 +0900

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,37 @@
 import { defineConfig } from "vite";
+import { importToCDN } from "vite-plugin-external-cdn";
 import tsconfigPaths from "vite-tsconfig-paths";
 import buildBlogList from "./scripts/buildBlogList";
 import copy404ToIndex from "./scripts/copy404ToIndex";
 
 export default defineConfig({
-  plugins: [tsconfigPaths(), buildBlogList(), copy404ToIndex()],
+  plugins: [
+    tsconfigPaths(),
+    buildBlogList(),
+    copy404ToIndex(),
+    importToCDN({
+      modules: [
+        {
+          name: "preact",
+          var: "preact",
+          path: "https://cdn.jsdelivr.net/npm/preact@10.19.3/dist/preact.min.js",
+        },
+        // {
+        //   name: "preact-router",
+        //   var: "preact-router",
+        //   path: "https://cdn.jsdelivr.net/npm/preact-router@4.1.0/dist/preact-router.min.js",
+        // },
+        {
+          name: "marked",
+          var: "marked",
+          path: "https://cdn.jsdelivr.net/npm/marked@11.1.1/lib/marked.umd.min.js",
+        },
+      ],
+    }),
+  ],
   build: {
     rollupOptions: {
-      external: ["preact", "preact-router", "marked"],
-    },
-    commonjsOptions: {
-      esmExternals: ["preact"],
+      external: ["preact", "marked"],
     },
   },
 });
