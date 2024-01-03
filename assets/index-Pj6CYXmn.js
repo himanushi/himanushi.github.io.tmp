5f197df642a9eab4a0762e56b974f2b4f9f9254c
himanushi <himanushi@users.noreply.github.com>
2024-01-03 05:05:26 +0000

    deploy: 86433d5788f3981e2102e65eae6f48e48bb4fac0

---
+++ assets/index-Pj6CYXmn.js
@@ -0,0 +1,4 @@
+(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const i of r.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&n(i)}).observe(document,{childList:!0,subtree:!0});function s(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function n(e){if(e.ep)return;e.ep=!0;const r=s(e);fetch(e.href,r)}})();(()=>{const t=document.querySelector("#app");t&&(t.innerHTML=`
+  <div>
+    <h1>Himanushi 雑記</h1>
+  </div>`)})();
