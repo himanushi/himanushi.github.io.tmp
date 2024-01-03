86433d5788f3981e2102e65eae6f48e48bb4fac0
himanushi <himanushi@users.noreply.github.com>
2024-01-03 14:02:38 +0900

    add lint

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
-import './style.css'
+import "./style.css";
 
 (() => {
-  document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  const html = document.querySelector<HTMLDivElement>("#app");
+
+  if (!html) return;
+
+  html.innerHTML = `
   <div>
     <h1>Himanushi 雑記</h1>
-  </div>`
-})()
+  </div>`;
+})();
