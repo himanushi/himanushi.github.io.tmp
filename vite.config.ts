d13cdc28a9d446ae4ae2d473678ae1091f6f626c
himanushi <himanushi@users.noreply.github.com>
2024-01-03 17:43:13 +0900

    外部CDNを使用する

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,9 @@ import copy404ToIndex from "./scripts/copy404ToIndex";
 
 export default defineConfig({
   plugins: [tsconfigPaths(), buildBlogList(), copy404ToIndex()],
+  build: {
+    rollupOptions: {
+      external: ["preact", "preact-router", "marked"],
+    },
+  },
 });
